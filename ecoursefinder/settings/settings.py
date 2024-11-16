9c20f6dc6556154a480e26ff7be9e84529819639
master <meetbhut2017@gmail.com>
2024-11-16 12:56:01 +0530

    startup project in system

--- ecoursefinder/settings/settings.py
+++ ecoursefinder/settings/settings.py
@@ -128,14 +128,14 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-STATIC_URL = '/static/'
-# STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+STATIC_URL = 'staticfiles/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'course_list/static'),
 # ]
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'staticfiles'),
-#]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'staticfiles'),
+]
